bef50315d22534d6d188c1c1ba660bba04372459
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-31 08:27:51 +0900

    apply lint and formatter

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 
 const Counter = () => {
   const [count, setCount] = React.useState(0);
+  const increment = () => setCount(count + 1);
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={increment}>Click me</button>
     </div>
   );
 };
