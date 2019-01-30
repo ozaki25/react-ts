f3e4735efcc11e60852d9f5436ecb109774b5121
ozaki <fs65ugumori@yahoo.co.jp>
2019-01-31 08:05:13 +0900

    add hooks sample components

---
+++ src/components/Counter.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+const Counter = () => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
+
+export default Counter;
