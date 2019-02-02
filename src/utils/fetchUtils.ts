57e51ccd1e804ae36a1c8fb61be473a9fd013383
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-03 03:02:10 +0900

    add fetch articles

---
+++ src/utils/fetchUtils.ts
@@ -0,0 +1,9 @@
+import { Article } from '../types/Article';
+
+const api = 'https://qiita.com/api/v2/users/ozaki25/items';
+
+export const fetchArticles = async (): Promise<Article[]> => {
+  const articles = await fetch(api).then(res => res.json());
+  console.table(articles);
+  return articles;
+};
