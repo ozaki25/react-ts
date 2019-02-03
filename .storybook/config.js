2435dbdaeaad9f5cd1217ba7f648c02e78cc2e26
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-04 08:15:16 +0900

    add storybook info

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
+import { withInfo } from '@storybook/addon-info';
 
 addDecorator(withKnobs);
+addDecorator(withInfo);
 
 const req = require.context('../src/stories', true, /.stories.tsx$/);
 
