b00351542314ea96e49908a2557df00da3e3e0b9
ozaki <fs65ugumori@yahoo.co.jp>
2019-02-05 08:38:08 +0900

    add redux boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React, { Suspense } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import Articles from 'src/components/Articles';
 import Counter from 'src/components/Counter';
 import Hello from 'src/components/Hello';
 import Loading from 'src/components/Loading';
 
+import reducer from 'src/modules/reducer';
+
+const store = createStore(reducer);
+
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Hello />
       <Suspense fallback={<Loading />}>
         <Articles />
       </Suspense>
-    </>
+    </Provider>
   );
 }
 
